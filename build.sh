09f1b1f7a1f46f9b58a720858e3e18e614089070
fukuta-tatsuya-intec <fukuta_tatsuya@intec.co.jp>
2021-02-06 21:10:49 +0900

    test

--- build.sh
+++ build.sh
@@ -2,15 +2,9 @@
 
 set -x
 
-architecture=""
-case $(uname -m) in
-    i386)   architecture="386" ;;
-    i686)   architecture="386" ;;
-    x86_64) architecture="amd64" ;;
-    arm)    dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
-esac
+architecture=$(dpkg --print-architecture)
 
-if [ $architecture = "arm" ]; then
+if [ "$architecture" = "armhf" ]; then
   apt-get install -y --no-install-recommends \
                                   apt-transport-https \
                                   ca-certificates \
