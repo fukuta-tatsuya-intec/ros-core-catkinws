988ce6d6205f0bad9afef6225452ee687f3339aa
fukuta-tatsuya-intec <fukuta_tatsuya@intec.co.jp>
2021-02-06 21:23:52 +0900

    test

--- build.sh
+++ build.sh
@@ -13,5 +13,5 @@ if [ "$architecture" = "armhf" ]; then
                                   wget
   wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null && \
   apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main' && \
-  apt-get update && apt-get install -y cmake
+  apt-get update && apt-get install -y cmake=3.19.2
 fi
